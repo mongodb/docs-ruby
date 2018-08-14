mongodb/docs

Fix build on linux.
Rsync-like operation of cp must be an osx feature, linux needs the asterisk

Makefile
@@ -63,10 +63,10 @@ migrate: get-assets
 	mkdir ${TARGET_DIR}
 	
 	@echo "Copying over ruby-driver docs files"
-	cp -R build/ruby-driver-${GIT_BRANCH}/docs/ ${TARGET_DIR}
+	cp -R build/ruby-driver-${GIT_BRANCH}/docs/* ${TARGET_DIR}
 	
 	@echo "Copying over bson  docs files"
-	cp -R build/bson-ruby/docs/ ${TARGET_DIR}	
+	cp -R build/bson-ruby/docs/* ${TARGET_DIR}
 	
 get-assets:
 	giza generate assets

mongodb/docs-ruby

Makefile
@@ -67,10 +67,10 @@ migrate: get-assets
 	mkdir ${TARGET_SOURCE}
 	
 	@echo "Copying over ruby-driver docs files"
-	cp -R build/ruby-driver-${GIT_BRANCH}/docs/ ${TARGET_SOURCE}
+	cp -R build/ruby-driver-${GIT_BRANCH}/docs/* ${TARGET_SOURCE}
 	
 	@echo "Copying over bson  docs files"
-	cp -R build/bson-ruby/docs/ ${TARGET_SOURCE}	
+	cp -R build/bson-ruby/docs/* ${TARGET_SOURCE}	
 
 get-assets:
 	giza generate assets