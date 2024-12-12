c05e8959b69c95a5c5448218d9acc1f9c2761893
norareidy <nora.reidy@mongodb.com>
2024-12-12 16:38:23 -0500

    JB feedback

--- source/includes/read/retrieve.rb
+++ source/includes/read/retrieve.rb
@@ -7,9 +7,7 @@ end
 
 uri = '<connection string>'
 
-begin
-  client = Mongo::Client.new(uri)
-
+Mongo::Client.new(uri) do |client|
   # start-db-coll
   database = client.use('sample_training')
   collection = database[:companies]
@@ -17,13 +15,13 @@ begin
 
   # Finds one document with a "name" value of "LinkedIn"
   # start-find-one
-  document = collection.find({ 'name' => 'LinkedIn' }).first
+  document = collection.find(name: 'LinkedIn').first
   puts document
   # end-find-one
 
   # Finds documents with a "founded_year" value of 1970
   # start-find-many
-  results = collection.find({ 'founded_year' => 1970 })
+  results = collection.find(founded_year: 1970)
   # end-find-many
 
   # start-cursor
@@ -35,13 +33,11 @@ begin
 
   # Finds and prints up to 2 documents with a "number_of_employees" value of 1000
   # start-modify
-  limit_results = collection.find({ 'number_of_employees' => 1000 }).limit(2)
+  limit_results = collection.find(number_of_employees: 1000).limit(2)
 
   limit_results.each do |doc|
     puts doc
   end
   # end-modify
-ensure
-  client&.close
 end
 
