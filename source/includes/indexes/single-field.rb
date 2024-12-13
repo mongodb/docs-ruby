565986fbc55913b24c5333d32fc622a05a856a51
Lindsey Moore <lindsey.moore@mongodb.com>
2024-12-13 18:57:10 -0500

    Stephanie's reiview

--- source/includes/indexes/single-field.rb
+++ source/includes/indexes/single-field.rb
@@ -3,10 +3,10 @@ require 'mongo'
 # Replace the placeholders with your credentials
 uri = "<connection string>"
 
-# Set the server_api field of the options object to Stable API version 1
+# Sets the server_api field of the options object to Stable API version 1
 options = { server_api: { version: "1" }}
 
-# Create a new client and connect to the server
+# Creates a new client and connect to the server
 client = Mongo::Client.new(uri, options)
 
 # start-sample-data
@@ -15,13 +15,12 @@ collection = database[:movies]
 # end-sample-data
 
 # start-index-single
-# Create an index on the "title" field
+# Creates an index on the "title" field
 index = collection.indexes.create_one({ title: 1 })
-puts "Index created: #{index}"
 # end-index-single
 
 # start-index-single-query
-# Find a document with the title "Sweethearts"
+# Finds a document with the title "Sweethearts"
 filter = { title: 'Sweethearts' }
 doc = collection.find(filter).first
 
@@ -33,7 +32,7 @@ end
 # end-index-single-query
 
 # start-check-single-index
-# List all indexes on the collection
+# Lists all indexes on the collection
 all_indexes = collection.indexes
 all_indexes.each do |index_spec|
   puts index_spec.to_json
