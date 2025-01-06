f2a127349dd048dd50061e34df5f53e4207d3d3e
Stephanie Aurelio <stephanie.aurelio@mongodb.com>
2025-01-06 15:18:36 -0800

    fix indentation

--- source/includes/write/transaction.rb
+++ source/includes/write/transaction.rb
@@ -7,39 +7,39 @@ end
 uri = "<connection string URI>"
 
 Mongo::Client.new(uri) do |client|
-    #start-txn
-    database = client.use('sample_mflix')
-    movies_collection = database[:movies]
-    users_collection = database[:users]
+  #start-txn
+  database = client.use('sample_mflix')
+  movies_collection = database[:movies]
+  users_collection = database[:users]
   
-    def run_transaction(session, movies_collection, users_collection)
-      transaction_options = {
-        read_concern: { level: "snapshot" },
-        write_concern: { w: "majority" }
-      }
+  def run_transaction(session, movies_collection, users_collection)
+    transaction_options = {
+      read_concern: { level: "snapshot" },
+      write_concern: { w: "majority" }
+    }
   
-      session.with_transaction(transaction_options) do
-        # Inserts document into the "movies" collection
-        insert_result = movies_collection.insert_one({ name: 'The Menu', runtime: 107 }, session: session)
-        puts "Insert completed: #{insert_result.inspect}"
+  session.with_transaction(transaction_options) do
+    # Inserts document into the "movies" collection
+    insert_result = movies_collection.insert_one({ name: 'The Menu', runtime: 107 }, session: session)
+    puts "Insert completed: #{insert_result.inspect}"
   
-        # Updates document in the "users" collection
-        update_result = users_collection.update_one({ name: 'Amy Phillips'}, { "$set" => { name: 'Amy Ryan' }}, session: session)
-        puts "Update completed: #{update_result.inspect}"
-      end
+    # Updates document in the "users" collection
+    update_result = users_collection.update_one({ name: 'Amy Phillips'}, { "$set" => { name: 'Amy Ryan' }}, session: session)
+    puts "Update completed: #{update_result.inspect}"
     end
-  
-    # Starts a session
-    session = client.start_session
-  
-    begin
-      # Runs the transaction
-      run_transaction(session, movies_collection, users_collection)
-      puts "Transaction committed successfully."
-    rescue Mongo::Error::OperationFailure => e
-      puts "Transaction failed and was aborted. Error: #{e.message}"
-    ensure
-      session.end_session
-    end
-    #end-txn
   end
+  
+  # Starts a session
+  session = client.start_session
+  
+  begin
+    # Runs the transaction
+    run_transaction(session, movies_collection, users_collection)
+    puts "Transaction committed successfully."
+  rescue Mongo::Error::OperationFailure => e
+    puts "Transaction failed and was aborted. Error: #{e.message}"
+  ensure
+    session.end_session
+  end
+  #end-txn
+end
