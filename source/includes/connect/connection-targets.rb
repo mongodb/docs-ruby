3f0e7a3f7e5e0d0dc657f4dc4e691f08751471cc
Lindsey Moore <lindsey.moore@mongodb.com>
2025-01-16 10:57:50 -0500

    stable api single quote

--- source/includes/connect/connection-targets.rb
+++ source/includes/connect/connection-targets.rb
@@ -5,7 +5,7 @@ require 'mongo'
 uri = "<connection string>"
 
 # Sets the server_api field of the options object to Stable API version 1
-options = { server_api: { version: "1" } }
+options = { server_api: { version: '1' } }
 # Creates a new client and connect to the server
 client = Mongo::Client.new(uri, options)
 # Sends a ping to confirm a successful connection
