a1a9897e6dcabc15d8927f5336703734a31f2048
Lindsey Moore <lindsey.moore@mongodb.com>
2025-01-27 15:20:11 -0500

    edits

--- source/includes/connect/csot.rb
+++ source/includes/connect/csot.rb
@@ -3,15 +3,17 @@ require 'mongo'
 
 # Replace the placeholder with your connection string
 uri = "<connection string>"
-options = { timeoutMS: 30000 }
 
+# Sets a client-level timeout configuration
+options = { timeoutMS: 30000 }
 client = Mongo::Client.new(uri, options)
 
 begin
   db = client.use('test-db')
   collection = db[:test-collection]
 
-  # Perform a query with an operation-level timeout configuration
+  # Perform a query with an operation-level timeout configuration,
+  # overriding the client-level configuration
   docs = collection.find({}, timeout_ms: 10000).to_a
 
   docs.each { |doc| puts doc }
