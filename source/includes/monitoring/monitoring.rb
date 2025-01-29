263143fa15bfceb8f685e7a056f6ea8a34251f76
Lindsey Moore <lindsey.moore@mongodb.com>
2025-01-29 15:02:14 -0500

    tech review comments

--- source/includes/monitoring/monitoring.rb
+++ source/includes/monitoring/monitoring.rb
@@ -11,77 +11,77 @@ client.subscribe( Mongo::Monitoring::SERVER_OPENING, subscriber )
 
 # start-sdam
 class SDAMLogSubscriber
-    include Mongo::Loggable
+  include Mongo::Loggable
+
+  def succeeded(event)
+    log_debug(format_event(event))
+  end
+
+  private
+
+  def logger
+    Mongo::Logger.logger
+  end
+
+  def format_message(message)
+    format("SDAM | %s", message)
+  end
+end
   
-    def succeeded(event)
-      log_debug(format_event(event))
-    end
-  
-    private
-  
-    def logger
-      Mongo::Logger.logger
-    end
-  
-    def format_message(message)
-      format("SDAM | %s".freeze, message)
+class TopologyOpeningLogSubscriber < SDAMLogSubscriber
+  private
+
+  def format_event(event)
+    "Topology type '#{event.topology.display_name}' initializing."
+  end
+end
+
+class ServerOpeningLogSubscriber < SDAMLogSubscriber
+  private
+
+  def format_event(event)
+    "Server #{event.address} initializing."
+  end
+end
+
+class ServerDescriptionChangedLogSubscriber < SDAMLogSubscriber
+  private
+
+  def format_event(event)
+    "Server description for #{event.address} changed from " +
+    "'#{event.previous_description.server_type}' to '#{event.new_description.server_type}'."
+  end
+end
+
+class TopologyChangedLogSubscriber < SDAMLogSubscriber
+  private
+
+  def format_event(event)
+    if event.previous_topology != event.new_topology
+      "Topology type '#{event.previous_topology.display_name}' changed to " +
+      "type '#{event.new_topology.display_name}'."
+    else
+      "There was a change in the members of the '#{event.new_topology.display_name}' " +
+      "topology."
     end
   end
-  
-  class TopologyOpeningLogSubscriber < SDAMLogSubscriber
-    private
-  
-    def format_event(event)
-      "Topology type '#{event.topology.display_name}' initializing."
-    end
+end
+
+class ServerClosedLogSubscriber < SDAMLogSubscriber
+  private
+
+  def format_event(event)
+    "Server #{event.address} connection closed."
   end
-  
-  class ServerOpeningLogSubscriber < SDAMLogSubscriber
-    private
-  
-    def format_event(event)
-      "Server #{event.address} initializing."
-    end
+end
+
+class TopologyClosedLogSubscriber < SDAMLogSubscriber
+  private
+
+  def format_event(event)
+    "Topology type '#{event.topology.display_name}' closed."
   end
-  
-  class ServerDescriptionChangedLogSubscriber < SDAMLogSubscriber
-    private
-  
-    def format_event(event)
-      "Server description for #{event.address} changed from " +
-      "'#{event.previous_description.server_type}' to '#{event.new_description.server_type}'."
-    end
-  end
-  
-  class TopologyChangedLogSubscriber < SDAMLogSubscriber
-    private
-  
-    def format_event(event)
-      if event.previous_topology != event.new_topology
-        "Topology type '#{event.previous_topology.display_name}' changed to " +
-        "type '#{event.new_topology.display_name}'."
-      else
-        "There was a change in the members of the '#{event.new_topology.display_name}' " +
-        "topology."
-      end
-    end
-  end
-  
-  class ServerClosedLogSubscriber < SDAMLogSubscriber
-    private
-  
-    def format_event(event)
-      "Server #{event.address} connection closed."
-    end
-  end
-  
-  class TopologyClosedLogSubscriber < SDAMLogSubscriber
-    private
-  
-    def format_event(event)
-      "Topology type '#{event.topology.display_name}' closed."
-    end
-  end
+end
 #end-sdam
 
 # start-sdam-subscriber-global
@@ -135,30 +135,30 @@ client = Mongo::Client.new(['127.0.0.1:27017'], database: 'test',
 
 # start-heartbeat
 class HeartbeatLogSubscriber
-    include Mongo::Loggable
-  
-    def started(event)
-      log_debug("#{event.address} | STARTED")
-    end
-  
-    def succeeded(event)
-      log_debug("#{event.address} | SUCCEEDED | #{event.duration}s")
-    end
-  
-    def failed(event)
-      log_debug("#{event.address} | FAILED | #{event.error.class}: #{event.error.message} | #{event.duration}s")
-    end
-  
-    private
-  
-    def logger
-      Mongo::Logger.logger
-    end
-  
-    def format_message(message)
-      format("HEARTBEAT | %s".freeze, message)
-    end
+  include Mongo::Loggable
+
+  def started(event)
+    log_debug("#{event.address} | STARTED")
   end
+
+  def succeeded(event)
+    log_debug("#{event.address} | SUCCEEDED | #{event.duration}s")
+  end
+
+  def failed(event)
+    log_debug("#{event.address} | FAILED | #{event.error.class}: #{event.error.message} | #{event.duration}s")
+  end
+
+  private
+
+  def logger
+    Mongo::Logger.logger
+  end
+
+  def format_message(message)
+    format("HEARTBEAT | %s", message)
+  end
+end
 # end-heartbeat
 
 # start-heartbeat-subscribe
@@ -168,6 +168,6 @@ subscriber = HeartbeatLogSubscriber.new
 Mongo::Monitoring::Global.subscribe(Mongo::Monitoring::SERVER_HEARTBEAT, subscriber)
 
 # Subscribes to Server Opening events at the client level
-client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'test' )
+client = Mongo::Client.new([ '127.0.0.1:27017' ], database: 'test' )
 client.subscribe( Mongo::Monitoring::SERVER_HEARTBEAT, subscriber )
 # end-heartbeat-subscribe
